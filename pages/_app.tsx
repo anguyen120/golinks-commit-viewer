f6a7dfd9e908b8825278f22820b6c31f9ea1a665
Alan Nguyen <23365399+anguyen120@users.noreply.github.com>
2023-02-12 00:32:38 -0600

    Added GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 import client from "../apollo-client"
+import { GoogleAnalytics } from "nextjs-google-analytics"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </ApolloProvider>
   )
